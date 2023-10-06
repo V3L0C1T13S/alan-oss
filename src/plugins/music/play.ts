945decdf3751f28f2d4207ebd021e83fb459e2d8
V3L0C1T13S <turtletester20@gmail.com>
2023-10-06 08:56:24 -0400

    plugins: remove music commands

--- src/plugins/music/play.ts
+++
@@ -1,9 +0,0 @@
-import { BaseCommand } from "../../common/index.js";
-
-export default class play extends BaseCommand {
-  async run() {
-    if (!this.channel || !this.guild) return "nah";
-
-    return "Ok";
-  }
-}
