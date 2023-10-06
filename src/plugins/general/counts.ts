646eb8690f3c3437a09a106c4be4a0ff3aca3ec7
V3L0C1T13S <turtletester20@gmail.com>
2023-10-06 12:26:33 -0400

    feat(plugins): counts command

--- src/plugins/general/counts.ts
+++ src/plugins/general/counts.ts
@@ -1,11 +1,18 @@
+import { EmbedBuilder } from "discord.js";
 import { BaseCommand } from "../../common/index.js";
 
 export default class counts extends BaseCommand {
+  static description = "Get statistics on popular commands.";
+
   async run() {
+    const commandCounts = await this.bot.database.getCounts();
+
+    const embed = new EmbedBuilder()
+      .setTitle("Counts")
+      .setDescription(Object.entries(commandCounts).map(([name, count]) => `**${name}** - ${count}`).join("\n"));
+
     return {
-      content: `${JSON.stringify(await this.bot.database.getCounts())}`,
+      embeds: [embed.toJSON()],
     };
   }
-
-  static description = "yes";
 }
