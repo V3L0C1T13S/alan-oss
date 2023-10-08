73b2358872e9fc785d3d234729fedd3c4e1aa476
V3L0C1T13S <turtletester20@gmail.com>
2023-10-08 15:18:39 -0400

    feat(plugins): add server avatar fetching

--- src/plugins/utilities/avatar.ts
+++ src/plugins/utilities/avatar.ts
@@ -6,16 +6,32 @@ export default class avatar extends BaseCommand {
     const user = this.args?.users?.[0] ?? this.author;
     if (!user.avatar) return ErrorMessages.AvatarNotFound;
 
+    if (this.args?.subcommands?.server) {
+      const member = await this.guild?.members.fetch(user);
+      if (member?.avatar) {
+        return this.clientType === "revolt"
+          ? `${revoltAutumnURL}/avatars/${member.avatar}`
+          : member.avatarURL() ?? ErrorMessages.AvatarNotFound;
+      }
+    }
+
+    if (!user.avatar) return ErrorMessages.AvatarNotFound;
+
     return this.clientType === "revolt"
       ? `${revoltAutumnURL}/avatars/${user.avatar}`
       : user.avatarURL() ?? ErrorMessages.AvatarNotFound;
   }
 
-  static description = "Get your (or someone elses) avatar URL";
+  static description = "Get your own (or someone elses) avatar URL";
   static parameters: CommandParameter[] = [{
     name: "user",
     description: "The user to get the avatar of.",
     type: CommandParameterTypes.User,
+    optional: true,
+  }, {
+    name: "server",
+    description: "Get the users server avatar.",
+    type: CommandParameterTypes.Bool,
     optional: true,
   }];
 }
