b88c480c87a80f5799e5dc59b4c2664d9abf1ff2
V3L0C1T13S <turtletester20@gmail.com>
2023-10-10 23:15:23 -0400

    refactor(db): reorganize database exports

--- src/common/utils/database/model/index.ts
+++ src/common/utils/database/model/index.ts
@@ -1,6 +1,8 @@
 import { Bot } from "Bot";
 import { type CommandCounts } from "./types.js";
 
+export * from "./types.js";
+
 export abstract class BaseDatabaseModel {
   bot: Bot;
 
@@ -8,13 +10,8 @@ export abstract class BaseDatabaseModel {
     this.bot = bot;
   }
 
-    abstract init(): Promise<void>;
-
-    abstract addCount(name: string): Promise<void>;
-
-    abstract getCounts(): Promise<CommandCounts>;
-
-    abstract stop(): Promise<void>;
+  abstract init(): Promise<void>;
+  abstract addCount(name: string): Promise<void>;
+  abstract getCounts(): Promise<CommandCounts>;
+  abstract stop(): Promise<void>;
 }
-
-export * from "./types.js";
