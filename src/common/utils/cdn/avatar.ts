ebba85ef682aa5fcfe183688258363d41ee12b80
V3L0C1T13S <turtletester20@gmail.com>
2023-10-11 12:12:31 -0400

    fix(utils): fix import

--- src/common/utils/cdn/avatar.ts
+++ src/common/utils/cdn/avatar.ts
@@ -1,6 +1,6 @@
 import { GuildMember, User } from "discord.js";
 import { ClientType } from "../../types/index.js";
-import { revoltAutumnURL } from "../../../constants/index";
+import { revoltAutumnURL } from "../../../constants/index.js";
 
 export function getUserAvatarURL(user: User | GuildMember, type: ClientType) {
   if (type === "revolt") {
