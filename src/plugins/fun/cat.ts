8a69e89527e144f49d92e84e88a98fcbf9c711aa
V3L0C1T13S <turtletester20@gmail.com>
2023-10-12 19:40:36 -0400

    feat(plugins): add text option to cat plugin

--- src/plugins/fun/cat.ts
+++ src/plugins/fun/cat.ts
@@ -1,15 +1,25 @@
 import axios from "axios";
 import { EmbedBuilder } from "discord.js";
-import { BaseCommand, CataasResponse } from "../../common/index.js";
+import {
+  BaseCommand, CataasResponse, CommandParameter, CommandParameterTypes,
+} from "../../common/index.js";
 
 const cataasURL = "https://cataas.com";
+const restrictedCharRegex = /~!\$&@#\*\(\)=:\/,;\?\+/gsi;
 
 export default class Cat extends BaseCommand {
   static description = "Get an image of a cat.";
+  static parameters: CommandParameter[] = [{
+    name: "text",
+    description: "Make the cat say something!",
+    optional: true,
+    type: CommandParameterTypes.String,
+  }];
 
   async run() {
-    const catInfo = await axios.get<CataasResponse>(`${cataasURL}/cat?json=true`);
-
+    const text = this.args?.subcommands?.text?.toString() ?? this.joinArgsToString();
+    const cleanText = text?.replaceAll(restrictedCharRegex, "");
+    const catInfo = await axios.get<CataasResponse>(`${cataasURL}/cat${cleanText ? `/says/${encodeURIComponent(cleanText)}` : ""}?json=true`);
     const catURL = `${cataasURL}${catInfo.data.url}`;
 
     const embed = new EmbedBuilder()
