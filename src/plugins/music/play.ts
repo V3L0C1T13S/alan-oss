ed9c2841b0da5f4547ed54ea5f0aa40fcbff92ba
V3L0C1T13S <turtletester20@gmail.com>
2023-10-16 12:23:38 -0400

    feat(plugins): play command

---
+++ src/plugins/music/play.ts
@@ -0,0 +1,28 @@
+import { ErrorMessages } from "../../constants/index.js";
+import { BaseCommand, CommandParameter, CommandParameterTypes } from "../../common/index.js";
+
+export default class Play extends BaseCommand {
+  static description = "Play some tunes!";
+  static parameters: CommandParameter[] = [{
+    name: "url",
+    description: "A URL to the music.",
+    type: CommandParameterTypes.String,
+    optional: false,
+  }];
+
+  async run() {
+    const soundURL = this.args?.subcommands?.url?.toString() ?? this.joinArgsToString();
+    if (!soundURL) return ErrorMessages.NotEnoughArgs;
+
+    const player = this.bot.soundPlayerManager.getPlayer(this.clientType);
+    if (!player) return "No player?";
+    if (!this.guild) return "No guild.";
+
+    const member = await this.guild.members.fetch(this.author);
+
+    return player.play(soundURL, {
+      guild: this.guild,
+      member,
+    });
+  }
+}
