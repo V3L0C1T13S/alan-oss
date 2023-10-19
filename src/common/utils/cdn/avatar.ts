b882b3ca01e0e07f99f72878a6f8052843a37fa2
V3L0C1T13S <turtletester20@gmail.com>
2023-10-19 09:55:26 -0400

    feat(utils): add getUserBannerURL refactor(plugins): use cdnutils in banner

--- src/common/utils/cdn/avatar.ts
+++ src/common/utils/cdn/avatar.ts
@@ -11,3 +11,13 @@ export function getUserAvatarURL(user: User | GuildMember, type: ClientType) {
 
   return user.avatarURL();
 }
+
+export function getUserBannerURL(user: User, type: ClientType) {
+  if (type === "revolt") {
+    if (!user.banner) return;
+
+    return `${revoltAutumnURL}/backgrounds/${user.banner}`;
+  }
+
+  return user.bannerURL();
+}
--- src/plugins/utilities/banner.ts
+++ src/plugins/utilities/banner.ts
@@ -1,14 +1,13 @@
-import { Messages, revoltAutumnURL } from "../../constants/index.js";
-import { CommandParameter, CommandParameterTypes, BaseCommand } from "../../common/index.js";
+import { Messages } from "../../constants/index.js";
+import {
+  CommandParameter, CommandParameterTypes, BaseCommand, getUserBannerURL,
+} from "../../common/index.js";
 
 export default class banner extends BaseCommand {
   async run() {
     const user = this.args?.users?.[0] ?? this.author;
-    if (!user.banner) return Messages.BannerNotFound;
 
-    return this.clientType === "revolt"
-      ? `${revoltAutumnURL}/backgrounds/${user.banner}`
-      : user.bannerURL() ?? Messages.BannerNotFound;
+    return getUserBannerURL(user, this.clientType) ?? Messages.BannerNotFound;
   }
 
   static description = "Get your (or someone elses) banner URL";
