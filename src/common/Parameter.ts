8aebdb8655e8a81987a3bb857727886a26cfa073
V3L0C1T13S <turtletester20@gmail.com>
2023-10-24 12:18:19 -0400

    refactor(utils): better command parameter typings

--- src/common/Parameter.ts
+++ src/common/Parameter.ts
@@ -13,20 +13,36 @@ export interface ParameterChoice {
   value: string,
 }
 
-export interface BaseCommandParameter {
+export interface BaseCommandParameter<T extends CommandParameterTypes> {
   /**
    * @maxlength 32
   * */
   name: string;
-  type: CommandParameterTypes;
+  type: T;
   description: string;
   optional?: boolean;
+}
+
+export interface ChoiceCommandParameter<
+  T extends CommandParameterTypes
+> extends BaseCommandParameter<T> {
   choices?: ParameterChoice[],
 }
 
-export interface SubCommandParameter extends Omit<BaseCommandParameter, "optional"> {
-  type: CommandParameterTypes.Subcommand,
-  subcommands: BaseCommandParameter[]
+export type StringCommandParameter = ChoiceCommandParameter<CommandParameterTypes.String>;
+export type NumberCommandParameter = BaseCommandParameter<CommandParameterTypes.Number>;
+export type BoolCommandParameter = BaseCommandParameter<CommandParameterTypes.Bool>;
+export type UserCommandParameter = BaseCommandParameter<CommandParameterTypes.User>;
+export type ChannelCommandParameter = BaseCommandParameter<CommandParameterTypes.Channel>;
+export type AttachmentCommandParameter = BaseCommandParameter<CommandParameterTypes.Attachment>;
+export interface SubCommandParameter extends Omit<BaseCommandParameter<CommandParameterTypes.Subcommand>, "optional"> {
+  subcommands: BaseCommandParameter<CommandParameterTypes>[]
 }
 
-export type CommandParameter = BaseCommandParameter | SubCommandParameter;
+export type CommandParameter = StringCommandParameter
+  | NumberCommandParameter
+  | BoolCommandParameter
+  | UserCommandParameter
+  | ChannelCommandParameter
+  | AttachmentCommandParameter
+  | SubCommandParameter;
