d098b185b21cc7692f3455500cc9b160397c263b
V3L0C1T13S <turtletester20@gmail.com>
2023-11-11 12:02:41 -0500

    fix(db): make user accounts not required

--- src/common/utils/database/impl/mongo/user.ts
+++ src/common/utils/database/impl/mongo/user.ts
@@ -14,7 +14,10 @@ const AccountSchema = new Schema({
 
 export const UserSchema = new Schema<DbUser>({
   id: String,
-  accounts: AccountSchema,
+  accounts: {
+    type: AccountSchema,
+    required: false,
+  },
   accepted_ai_tos: {
     type: Boolean,
     required: false,
